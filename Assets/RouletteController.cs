fe649d69bbcbf0932374561fd1386eb7d10bdb52
steel <gawct.231020@gmail.com>
2024-03-05 12:03:50 +0900

    RouletteController作成

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed=0;//回転速度
+    // Start is called before the first frame update
+    void Start()
+    {
+        //フレームレートを60に固定する
+        Application.targetFrameRate=60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスが押されたら回転速度を設定する
+        if(Input.GetMouseButtonDown(0)){
+            this.rotSpeed=10;
+        }        
+        //回転速度ぶん、ルーレットを回転させる
+        transform.Rotate(0,0,this.rotSpeed);
+    }
+}
